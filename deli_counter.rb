66fea9254338f437eb2ef5fb6a1f1435823b292d
Dominique Skaf <doskaf@yahoo.com>
2019-09-13 17:23:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif katz_deli.size > 0
+    new_array = []
+    counter = 1
+    katz_deli.each do |name|
+      new_array << "#{counter}. #{name}"
+      counter += 1
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  end
+end
+
+def take_a_number
+  
+end
